d2b860ccc09f871ca6b2526ef3f4ab538174feee
wangkefu <745376631@qq.com>
2019-01-07 15:48:08 +0800

    updat

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,74 @@
+<template>
+	<ul class="list">
+		<li class="item" v-for="item of letters" 
+			:key="item" 
+			:ref="item"
+			@touchstart="handleTouchStart"
+			@touchmove="handleTouchMove"
+			@touchend="handleTouchEnd"
+			@click="handleLetterClick">
+			{{item}}
+		</li>
+	</ul>
+</template>
+
+<script>
+export default {
+	name: 'CityAlphabet',
+	props: {
+		cities: Object
+	},
+	computed: {
+		letters () {
+			const letters = []
+			for(let i in this.cities) {
+				letters.push(i)
+			}
+			return letters
+		}
+	},
+	data () {
+		return {
+			touchStatus: false
+		}
+	},
+	methods: {
+		handleLetterClick (e) {
+			this.$emit('change', e.target.innerText)
+		},
+		handleTouchStart () {
+			this.touchStatus = true
+		},
+		handleTouchMove (e) {
+			if(this.touchStatus) {
+				const startY = this.$refs['A'][0].offsetTop
+				const touchY = e.touches[0].clientY - 79
+				const index = Math.floor((touchY - startY) / 20)
+				if (index >= 0  && index < this.letters.length) {
+					this.$emit('change', this.letters[index])
+				}
+			}
+		},
+		handleTouchEnd () {
+
+		}
+	}
+}	
+</script>
+
+<style lang="stylus" scoped>
+	@import '~styles/varibles.styl'
+	.list
+		display: flex
+		flex-direction: column
+		justify-content: center
+		position: absolute
+		right: 0
+		top: 1.58rem
+		bottom: 0
+		width: .4rem
+		.item
+			text-align: center
+			line-height: .4rem
+			color: $bgColor
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/pages/home/Home.vue'
+import City from '@/pages/city/City.vue'
 // @符币表示src目录
 
 Vue.use(Router)
@@ -11,6 +12,10 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home
+    }, {
+      path: '/city',
+      name: 'City',
+      component: City
     }
   ]
 })

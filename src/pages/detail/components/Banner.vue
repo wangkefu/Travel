20f708c503e559e1eb71136285c821220b711e18
wangkefu <745376631@qq.com>
2019-08-31 14:46:03 +0800

    update

---
+++ src/pages/detail/components/Banner.vue
@@ -0,0 +1,72 @@
+<template>
+    <div>
+        <div class="banner" @click="handleBannerClick">
+            <img class="banner-img" src="//img1.qunarzz.com/sight/p0/1903/61/617ac4a711f6cb34a3.img.jpg_600x330_e2ae600a.jpg" alt="">
+            <div class="banner-info">
+                <div class="banner-tittle">苏州水乐园</div>
+                <div class="banner-number"><span class="iconfont banner-icon">&#xe632;</span>39</div>
+            </div>
+        </div>
+        <common-gallary 
+            :imgs="imgs" 
+            v-show="showGallary"
+            @close="handleGallaryClose"></common-gallary>
+    </div>
+</template>
+
+<script>
+import CommonGallary from 'common/gallary/Gallary'
+export default {
+    name: 'DetailBanner',
+    data () {
+        return {
+            showGallary: false,
+            imgs: ['http://img1.qunarzz.com/sight/p0/1903/ca/ca62c9be792c852ba3.img.jpg_r_800x800_971cf21d.jpg','http://img1.qunarzz.com/sight/p0/1903/14/14c12f0c45c4c5a2a3.img.jpg_r_800x800_e012147a.jpg']
+        }
+    },
+    methods: {
+        handleBannerClick() {
+            this.showGallary = true
+        },
+        handleGallaryClose() {
+            this.showGallary = false
+        }
+    },
+    components: {
+        CommonGallary
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+    .banner
+        position: relative
+        overflow: hidden
+        height: 0
+        padding-bottom: 55%
+        .banner-img
+            width:100%
+        .banner-info
+            display: flex
+            position: absolute
+            left: 0
+            right: 0
+            bottom: 0
+            line-height: .6rem 
+            color: #fff
+            background-image: linear-gradient(top, rgba(0,0,0,0), rgba(0,0,0,0.8))
+            .banner-tittle
+                flex: 1
+                font-size: .32rem
+                padding: 0 .2rem
+            .banner-number
+                height: .32rem
+                line-height: .32rem
+                margin-top: .14rem
+                padding: 0 .4rem
+                border-radius: .2rem
+                background: rgba(0, 0, 0, .8)
+                font-size: .24rem
+                .banner-icon
+                    font-size: .24rem
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/pages/home/Home.vue'
 import City from '@/pages/city/City.vue'
+import Detail from '@/pages/detail/Detail'
 // @符币表示src目录
 
 Vue.use(Router)
@@ -16,6 +17,10 @@ export default new Router({
       path: '/city',
       name: 'City',
       component: City
+    }, {
+      path: '/detail/:id',  //动态路由  id为参数
+      name: 'Detail',
+      component: Detail
     }
   ]
 })
